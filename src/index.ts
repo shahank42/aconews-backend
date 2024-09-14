3315edb745d58442844371d05d23c6be6503fd4d
shahank42 <shounakghosh42@gmail.com>
2024-09-14 16:23:10 +0530

    feat: GNews Request

---
+++ src/index.ts
@@ -0,0 +1,55 @@
+import { Hono } from "hono";
+import { z } from "zod";
+import type { GNewsResponse } from "./types";
+
+type Bindings = {
+	GNEWS_API_KEY: string;
+};
+
+const app = new Hono<{ Bindings: Bindings }>();
+
+const NewsQuerySchema = z.object({
+	category: z.string(),
+	country: z.string(),
+	language: z.string(),
+	max: z.string(),
+});
+
+app.get("/news", async (c) => {
+	try {
+		const result = NewsQuerySchema.safeParse(c.req.query());
+		if (!result.success) {
+			return c.json({ error: "Invalid query parameters" }, 400);
+		}
+
+		const { category, country, language, max } = result.data;
+
+		const url = new URL("https://gnews.io/api/v4/top-headlines");
+		const params = {
+			apikey: c.env.GNEWS_API_KEY,
+			category,
+			lang: language,
+			country,
+			max,
+		};
+		for (const [key, value] of Object.entries(params)) {
+			url.searchParams.append(key, value);
+		}
+
+		const res = await fetch(url.toString());
+		if (!res.ok) {
+			throw new Error(`API responded with status: ${res.status}`);
+		}
+
+		const data = (await res.json()) as GNewsResponse;
+		return c.json(data.articles);
+	} catch (error) {
+		console.error("News fetch from GNews failed:", error);
+		return c.json(
+			{ status: "ERROR", message: "News fetch from GNews failed" },
+			500,
+		);
+	}
+});
+
+export default app;
---
+++ src/types.ts
@@ -0,0 +1,14 @@
+export type GNewsResponse = {
+  articles: {
+    title: string;
+    description: string;
+    content: string;
+    url: string;
+    image: string;
+    publishedAt: string;
+    source: {
+      name: string;
+      url: string;
+    }
+  }[]
+}
